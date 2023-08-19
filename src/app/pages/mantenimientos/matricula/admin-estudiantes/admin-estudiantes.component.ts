9aff33cf09d17a7f3657126dfdfe87ffe12e511f
Joxini <jocksanvargas15@gmail.com>
2023-08-18 20:50:06 -0600

    Ya pone datos de estudiante seleccionado

--- src/app/pages/mantenimientos/matricula/admin-estudiantes/admin-estudiantes.component.ts
+++ src/app/pages/mantenimientos/matricula/admin-estudiantes/admin-estudiantes.component.ts
@@ -1,10 +1,62 @@
 import { Component } from '@angular/core';
-
+import { MatDialog } from '@angular/material/dialog';
+import { MatTableDataSource } from '@angular/material/table';
+import { ToastrService } from 'ngx-toastr';
+import { Estudiante } from 'src/app/shared/models/estudiante';
+import { EstudianteService } from 'src/app/shared/services/estudiante.service';
+import { MatDialogRef } from '@angular/material/dialog';
 @Component({
   selector: 'app-admin-estudiantes',
   templateUrl: './admin-estudiantes.component.html',
   styleUrls: ['./admin-estudiantes.component.scss']
 })
 export class AdminEstudiantesComponent {
+
+  titulo = 'SELECCIONE EL ESTUDIANTE';
+  displayedColumns: string[] = [
+    'IdEstudiante',
+    'Nombre',
+    'Apellido1',
+    'Apellido2',
+    'FechaNah',
+    'Genero',
+    'acciones'
+  ];
+
+  dataSource = new MatTableDataSource();
+
+  constructor(
+    private srvEstudiantes: EstudianteService,
+    public dialog: MatDialog,
+    private mensajeria: ToastrService,
+    private dialogRef: MatDialogRef<AdminEstudiantesComponent>
+  ) {}
+  ngOnInit() {
+    this.cargarlista()
+  }
+
+  cargarlista(){
+    this.srvEstudiantes.getAll().subscribe(
+      (datos) => {
+        // console.log(datos);
+        this.dataSource.data = datos;
+      },
+      (error) => {
+        this.mensajeria.error(error);
+      }
+    );
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  seleccionar(student: any): void {
+    this.dialogRef.close(student);
+  }
+
+
+  
 
 }
--- src/app/pages/mantenimientos/matricula/matricula.component.ts
+++ src/app/pages/mantenimientos/matricula/matricula.component.ts
@@ -1,11 +1,61 @@
-import { Component } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialog } from '@angular/material/dialog';
+import { ToastrService } from 'ngx-toastr';
+import {EstudianteForm  } from 'src/app/shared/formsModels/estudianteForms';
+import { Estudiante } from 'src/app/shared/models/estudiante';
+import { CursosService } from 'src/app/shared/services/cursos.service';
+import { EstudianteService } from 'src/app/shared/services/estudiante.service';
+import { AdminEstudiantesComponent } from './admin-estudiantes/admin-estudiantes.component';
+import { MatTableDataSource } from '@angular/material/table';
+import { formatDate } from '@angular/common';
 @Component({
   selector: 'app-matricula',
   templateUrl: './matricula.component.html',
   styleUrls: ['./matricula.component.scss']
 })
-export class MatriculaComponent {
-  titulo = 'Crear Producto';
 
-}
+export class MatriculaComponent {
+
+  selectedStudent: Estudiante
+  dataSource = new MatTableDataSource();
+  constructor(
+    public estudianteform: EstudianteForm,
+    private srvEstudiante: EstudianteService,
+    public dialog: MatDialog,
+    private mensajeria: ToastrService,
+  ) {}
+
+  abrirDialog(estudiante?: Estudiante): void {
+      const dialogOpen = this.dialog.open(AdminEstudiantesComponent, {
+        width: '800px',
+        height: '800px',
+        data: { estudiante },
+      });
+
+      dialogOpen.afterClosed().subscribe((estudiante: any) => {
+        if (estudiante) {
+          this.selectedStudent = estudiante;
+          this.cargarDatosForm();
+          console.log("Principal", this.selectedStudent)
+        }
+      });
+    } 
+
+
+    cargarDatosForm() {
+      this.estudianteform.baseForm.patchValue({
+        IdEstudiante: this.selectedStudent.IdEstudiante,
+        Nombre: this.selectedStudent.Nombre,
+        Apellido1: this.selectedStudent.Apellido1,
+        Apellido2: this.selectedStudent.Apellido2,
+        FechaNah: formatDate(
+          this.selectedStudent.FechaNah,
+          'yyyy-MM-dd',
+          'en'
+        ),
+        Genero: this.selectedStudent.Genero,
+        estado: true,
+      });
+    }
+  }
+  
